4bf47b9a798528c3703b4891c1d6c2516355beb6
Rich <rich.cookson@gmail.com>
2017-07-20 16:08:41 +0100

    Working demo of inline videos in carousel without animated gif fallback

--- src/js/modules/carousel.js
+++ src/js/modules/carousel.js
@@ -3,45 +3,33 @@ define('carousel', ['jquery', 'flickity', 'polyfillPlaysInline'], function($, Fl
   'use strict';
 
   var polyfillVideos = function() {
-    console.log('Polyfill videos for iOS9 and 8');
-
+    //console.log('Polyfill videos for iOS9 and 8');
     $('.js-carousel').find('video').each(function () {
  	    enableInlineVideo(this);
     });
   }
 
   var initCarousel = function () {
-    console.log('Initialise carousel');
-
+    //console.log('Initialise carousel');
     var flkty = new Flickity( '.js-carousel', {
       adaptiveHeight: true,
       cellAlign: 'center',
       contain: true,
       initialIndex: 2
     });
-
   };
 
   var setActiveCarouselVideo = function() {
-    console.log('Initialise video behaviour');
-
-    // Pause all active videos
+    //console.log('Initialise video behaviour');
     $('.js-carousel').find('video').each( function( i, video ) {
-      console.log('Pause all active videos');
+      //console.log('Pause all active videos');
       video.pause();
     });
 
-    // Play currently is-selected video
     $('.js-carousel').find('.is-selected video').each( function( i, video ) {
-      console.log('Play video in selected slide on load');
+      //console.log('Play video in selected slide on load');
       video.play();
-      // var isPlaying = video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2;
-      //
-      // if (!isPlaying) {
-      //   video.play();
-      // }
     });
-
   };
 
   var init = function () {
@@ -51,11 +39,11 @@ define('carousel', ['jquery', 'flickity', 'polyfillPlaysInline'], function($, Fl
     setActiveCarouselVideo();
 
     // Events
+    // On Flickity slide change reset active carousel video
     $('.js-carousel').on( 'settle.flickity', function() {
-      console.log('Flickity settled on a new slide');
+      //console.log('Flickity settled on a new slide');
       setActiveCarouselVideo();
     });
-
   };
 
   return {
